a33282c018ffa57979cdb8f15b4e67e5793915d2
root <root@vmi1175516.contaboserver.net>
2025-04-17 17:53:07 +0200

    FIRST

---
+++ src/fastapi_shield/shield.py
@@ -0,0 +1,251 @@
+from typing_extensions import Doc
+from fastapi import Request, HTTPException, status
+
+from functools import wraps
+from inspect import signature, Signature, Parameter
+from enum import Enum
+
+from typing import (
+    Annotated,
+    Optional,
+    Callable,
+    Any,
+    Generic,
+    TypeVar,
+    Tuple,
+    Sequence,
+    Union,
+)
+
+T = TypeVar("T")
+U = TypeVar("U")
+
+
+class AuthenticationStatus(Enum):
+    AUTHENTICATED = "AUTHENTICATED"
+    UNAUTHENTICATED = "UNAUTHENTICATED"
+
+    def __bool__(self) -> bool:
+        return self is AuthenticationStatus.AUTHENTICATED
+
+
+EndPointFunc = Callable[..., Any]
+ShieldFunc = Callable[[T], Tuple[Union[AuthenticationStatus, bool], U]]
+
+
+class ShieldDepends:
+    def __init__(
+        self,
+        shielded_dependency: Optional[Callable[..., Any]] = None,
+    ):
+        self.shielded_dependency = shielded_dependency
+        self.authenticated = False
+
+    def __repr__(self) -> str:
+        return f"{type(self).__name__}(authenticated={self.authenticated}; shielded_dependency={self.shielded_dependency.__name__ if self.shielded_dependency else None})"
+
+    def __call__(self, *args, **kwargs):
+        if self.authenticated:
+            return self.shielded_dependency(*args, **kwargs)
+        return self
+
+
+def ShieldedDepends(  # noqa: N802
+    shielded_dependency: Annotated[
+        Optional[Callable[..., Any]],
+        Doc(
+            """
+            The dependency to be shielded.
+            """
+        ),
+    ] = None,
+) -> Any:
+    return ShieldDepends(shielded_dependency=shielded_dependency)
+
+
+def prepend_request_to_signature_of_endpoint(endpoint: EndPointFunc) -> Signature:
+    new_request_param: Parameter = Parameter(
+        name="request",
+        kind=Parameter.POSITIONAL_ONLY,
+        annotation=Request,
+        default=Parameter.empty,
+    )
+    new_signature = signature(endpoint)
+    new_params = [*[new_request_param], *new_signature.parameters.values()]
+    return Signature(new_params)
+
+
+def prepend_params_to_signature(
+    params: Sequence[Parameter], signature: Signature
+) -> Signature:
+    new_params = [*params, *signature.parameters.values()]
+    return Signature(new_params)
+
+
+def merge_dedup_seq_params(
+    *seqs_of_params: Sequence[Parameter],
+) -> list[Parameter]:
+    seen = {}
+    results = []
+    for seq_of_params in seqs_of_params:
+        for param in seq_of_params:
+            if param.name not in seen:
+                seen[param.name] = param
+                results.append(param)
+    return results
+
+
+def rearrange_params(seq_of_params: Sequence[Parameter]) -> list[Parameter]:
+    pos_only_params = [
+        param for param in seq_of_params if param.kind == Parameter.POSITIONAL_ONLY
+    ]
+    var_pos_params = [
+        param for param in seq_of_params if param.kind == Parameter.VAR_POSITIONAL
+    ]
+    kw_or_pos_params = [
+        param
+        for param in seq_of_params
+        if (
+            param.kind == Parameter.POSITIONAL_OR_KEYWORD
+            and param.default is param.empty
+        )
+    ] + [
+        param
+        for param in seq_of_params
+        if (
+            param.kind == Parameter.POSITIONAL_OR_KEYWORD
+            and param.default is not param.empty
+        )
+    ]
+    var_kw_params = [
+        param for param in seq_of_params if param.kind == Parameter.VAR_KEYWORD
+    ]
+    kw_only_params = [
+        param for param in seq_of_params if param.kind == Parameter.KEYWORD_ONLY
+    ]
+    rearranged_params = [
+        *pos_only_params,
+        *var_pos_params,
+        *kw_or_pos_params,
+        *var_kw_params,
+        *kw_only_params,
+    ]
+    return rearranged_params
+
+
+class Shield(Generic[T, U]):
+    def __init__(
+        self,
+        shield_func: ShieldFunc[T, U],
+        exception_to_raise_if_fail: HTTPException = HTTPException(
+            status_code=401, detail="Unauthorized"
+        ),
+    ):
+        assert callable(shield_func), "`shield_func` must be callable"
+        self._guard_func = shield_func
+        self._guard_func_params = signature(shield_func).parameters
+        assert isinstance(exception_to_raise_if_fail, HTTPException), (
+            "`exception_to_raise_if_fail` must be an instance of `HTTPException`"
+        )
+        self._exception_to_raise_if_fail = exception_to_raise_if_fail
+
+    def __call__(self, endpoint: EndPointFunc) -> EndPointFunc:
+        assert callable(endpoint), "`endpoint` must be callable"
+
+        @wraps(endpoint)
+        async def wrapper(*args, **kwargs):
+            guard_func_args = {
+                k: v for k, v in kwargs.items() if k in self._guard_func_params
+            }
+            auth_status, obj = self._guard_func(**guard_func_args)
+            if auth_status:
+                print(
+                    "`wrapper::inject_authenticated_entities_into_args_kwargs`: ",
+                    obj,
+                    args,
+                    kwargs,
+                )
+                args, endpoint_kwargs = inject_authenticated_entities_into_args_kwargs(
+                    obj, *args, **kwargs
+                )
+                endpoint_kwargs = {
+                    k: v
+                    for k, v in endpoint_kwargs.items()
+                    if k in signature(endpoint).parameters
+                }
+                return await endpoint(*args, **endpoint_kwargs)
+
+            raise self._exception_to_raise_if_fail
+
+        wrapper.__signature__ = Signature(
+            rearrange_params(
+                merge_dedup_seq_params(
+                    self._guard_func_params.values(),
+                    signature(endpoint).parameters.values(),
+                )
+            )
+        )
+        print("`wrapper::__signature__`: ", wrapper.__signature__)
+        return wrapper
+
+
+def get_values_from_kwargs_for_dependency(
+    dependency_func: Callable[[Any], Any], **kwargs
+):
+    dependency_func_params = signature(dependency_func).parameters
+    dependency_kwargs = {k: v for k, v in kwargs.items() if k in dependency_func_params}
+    return dependency_kwargs
+
+
+def inject_authenticated_entities_into_args_kwargs(
+    obj, *args, **kwargs
+) -> Tuple[Tuple[Any, ...], dict[str, Any]]:
+    authenticated_depends = search_args_kwargs_for_authenticated_depends(
+        *args, **kwargs
+    )
+    for idx_kw, arg_kwargs in authenticated_depends:
+        if idx_kw is not None:
+            if arg_kwargs.authenticated:
+                raise HTTPException(
+                    status.HTTP_500_INTERNAL_SERVER_ERROR,
+                    detail="Already authenticated",
+                )
+            arg_kwargs.authenticated = True
+            new_arg_kwargs = arg_kwargs.shielded_dependency(obj)
+            arg_kwargs.authenticated = False
+            if isinstance(idx_kw, int):
+                args = args[:idx_kw] + (new_arg_kwargs,) + args[idx_kw + 1 :]
+            if isinstance(idx_kw, str):
+                kwargs[idx_kw] = new_arg_kwargs
+    return args, kwargs
+
+
+def remove_authenticated_depends_from_signature_of_endpoint(
+    endpoint: EndPointFunc,
+) -> Signature:
+    new_signature = signature(endpoint)
+    return remove_authenticated_depends_from_signature(new_signature)
+
+
+def remove_authenticated_depends_from_signature(
+    signature: Signature,
+) -> Signature:
+    new_params = [
+        param
+        for param in signature.parameters.values()
+        if not isinstance(param.default, ShieldDepends)
+    ]
+    return Signature(new_params)
+
+
+def search_args_kwargs_for_authenticated_depends(
+    *args, **kwargs
+) -> list[Tuple[Union[str, int], ShieldDepends]]:
+    results = []
+    for idx, arg in enumerate(args):
+        if isinstance(arg, ShieldDepends):
+            results.append((idx, arg))
+    for kw, kwarg in kwargs.items():
+        if isinstance(kwarg, ShieldDepends):
+            results.append((kw, kwarg))
+    return results
---
+++ tests/test_basics.py
@@ -0,0 +1,233 @@
+from fastapi import FastAPI, Header, Request
+from fastapi.testclient import TestClient
+from fastapi_shield.shield import Shield, ShieldedDepends, AuthenticationStatus
+from fastapi import status
+from pydantic import BaseModel
+from typing import Tuple
+
+
+class User(BaseModel):
+    username: str
+    email: str
+    roles: list[str] = ["user"]
+
+
+FAKE_DB_DATA = {
+    "username1": User(
+        username="authenticated_user1",
+        email="authenticated_user1@example.com",
+        roles=["user", "admin"],
+    ),
+    "username2": User(
+        username="authenticated_user2",
+        email="authenticated_user2@example.com",
+    ),
+}
+
+
+def get_user(token: str) -> User:
+    if token == "valid_token1":
+        print("`get_user` returns `FAKE_DB_DATA['username1']`")
+        return FAKE_DB_DATA["username1"]
+    print("`get_user` returns `FAKE_DB_DATA['username2']`")
+    return FAKE_DB_DATA["username2"]
+
+
+def validate_token(token: str):
+    # Dummy token validation logic
+    print("`validate_token::token`: ", token)
+    if (token == "valid_token1") or (token == "valid_token2"):
+        print("`validate_token` returns `True`")
+        return True
+    print("`validate_token` returns `False`")
+    return False
+
+
+def get_auth_status_from_header(
+    *,
+    x_api_token: str = Header(),
+) -> Tuple[AuthenticationStatus, str]:
+    if validate_token(x_api_token):
+        return (AuthenticationStatus.AUTHENTICATED, x_api_token)
+    return (AuthenticationStatus.UNAUTHENTICATED, "")
+
+
+def get_auth_status(
+    request: Request,
+) -> Tuple[AuthenticationStatus, str]:
+    auth_header = request.headers.get("Authorization")
+    if not auth_header or not auth_header.startswith("Bearer "):
+        return AuthenticationStatus.UNAUTHENTICATED, ""
+    token = auth_header[len("Bearer ") :]
+    if validate_token(token):
+        return (AuthenticationStatus.AUTHENTICATED, token)
+    return (AuthenticationStatus.UNAUTHENTICATED, "")
+
+
+def allowed_user_roles(roles: list[str]):
+    def decorator(authenticated_user: User = ShieldedDepends(get_user)):
+        print("`allowed_user_roles::authenticated_user`: ", authenticated_user)
+        for role in roles:
+            if role in authenticated_user.roles:
+                return True, ""
+        return False, ""
+
+    return decorator
+
+
+# Define the FastAPI app
+app = FastAPI()
+auth_shield: Shield = Shield(get_auth_status)
+auth_api_shield: Shield = Shield(get_auth_status_from_header)
+roles_shield: Shield = lambda roles: Shield(allowed_user_roles(roles))
+
+
+# Unprotected endpoint
+@app.get("/unprotected")
+async def unprotected_endpoint(user: User = ShieldedDepends(get_user)):
+    return {"user": user, "message": "This is an unprotected endpoint"}
+
+
+# Protected endpoint
+@app.get("/protected")
+@auth_shield
+async def protected_endpoint(request: Request, user: User = ShieldedDepends(get_user)):
+    return {
+        "user": user,
+        "message": "This is a protected endpoint",
+    }
+
+
+# Protected endpoint
+@app.get("/protected4")
+@auth_shield
+@roles_shield(["admin"])
+async def protected_endpoint4(request: Request, user: User = ShieldedDepends(get_user)):
+    return {
+        "user": user,
+        "message": "This is a protected endpoint",
+    }
+
+
+@app.get("/protected-api")
+@auth_api_shield
+async def protected_endpoint3(
+    x_api_token: Header(), user: User = ShieldedDepends(get_user)
+):
+    return {
+        "x-api-token": x_api_token,
+        "user": user,
+        "message": "This is a protected endpoint",
+    }
+
+
+# Protected endpoint
+@app.get("/protected2")
+@auth_shield
+async def protected_endpoint2(
+    user1: User = ShieldedDepends(get_user),
+):
+    return {
+        "user1": user1,
+        "message": "This is a protected endpoint",
+    }
+
+
+# Test cases
+def test_endpoints():
+    client = TestClient(app)
+
+    # Test unprotected endpoint
+    response = client.get("/unprotected")
+    assert response.status_code == 200
+    assert response.json() == {
+        "message": "This is an unprotected endpoint",
+        "user": {"authenticated": False, "shielded_dependency": {}},
+    }, response.json()
+
+    # Test protected endpoint without token
+    response = client.get("/protected")
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED, response.status_code
+    assert response.json() == {"detail": "Unauthorized"}, response.json()
+
+    response = client.get("/protected2")
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED, response.status_code
+    assert response.json() == {"detail": "Unauthorized"}, response.json()
+
+    # Test protected endpoint with invalid token
+    response = client.get(
+        "/protected", headers={"Authorization": "Bearer invalid_token"}
+    )
+    assert response.status_code == 401, response.status_code
+    assert response.json() == {"detail": "Unauthorized"}, response.json()
+
+    response = client.get(
+        "/protected2", headers={"Authorization": "Bearer invalid_token"}
+    )
+    assert response.status_code == 401, response.status_code
+    assert response.json() == {"detail": "Unauthorized"}, response.json()
+
+    # Test protected endpoint with valid token
+    response = client.get(
+        "/protected", headers={"Authorization": "Bearer valid_token1"}
+    )
+    assert response.status_code == 200, response.status_code
+    assert response.json() == {
+        "user": {
+            "username": "authenticated_user1",
+            "email": "authenticated_user1@example.com",
+            "roles": ["user", "admin"],
+        },
+        "message": "This is a protected endpoint",
+    }, response.json()
+
+    response = client.get(
+        "/protected", headers={"Authorization": "Bearer uinvalid_token1"}
+    )
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED, response.status_code
+    assert response.json() == {"detail": "Unauthorized"}, response.json()
+
+    response = client.get(
+        "/protected2", headers={"Authorization": "Bearer valid_token1"}
+    )
+    assert response.status_code == 200, response.status_code
+    assert response.json() == {
+        "user1": {
+            "username": "authenticated_user1",
+            "email": "authenticated_user1@example.com",
+            "roles": ["user", "admin"],
+        },
+        "message": "This is a protected endpoint",
+    }, response.json()
+
+    response = client.get("/protected-api", headers={"X-API-Token": "valid_token1"})
+    assert response.status_code == 200, response.status_code
+    assert response.json() == {
+        "x-api-token": "valid_token1",
+        "user": {
+            "username": "authenticated_user1",
+            "email": "authenticated_user1@example.com",
+            "roles": ["user", "admin"],
+        },
+        "message": "This is a protected endpoint",
+    }, response.json()
+
+    response = client.get(
+        "/protected4", headers={"Authorization": "Bearer valid_token1"}
+    )
+    assert response.status_code == 200, response.status_code
+    assert response.json() == {
+        "user": {
+            "username": "authenticated_user1",
+            "email": "authenticated_user1@example.com",
+            "roles": ["user", "admin"],
+        },
+        "message": "This is a protected endpoint",
+    }, response.json()
+
+    response = client.get("/protected4", headers={"Authorization": "Bear valid_token2"})
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED, response.status_code
+    assert response.json() == {"detail": "Unauthorized"}, response.json()
+
+
+test_endpoints()
