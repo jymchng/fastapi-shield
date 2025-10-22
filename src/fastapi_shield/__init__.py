ec71e1622694a5960d049a0a5564d130e14a1883
Super User <root@vmi1175516.contaboserver.net>
2025-10-22 13:59:49 +0200

    chore: bump version to 0.1.5

--- src/fastapi_shield/__init__.py
+++ src/fastapi_shield/__init__.py
@@ -32,6 +32,6 @@ For more information, visit: https://github.com/jymchng/fastapi-shield
 
 from fastapi_shield.shield import Shield, ShieldedDepends, shield
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 
 __all__ = ["Shield", "ShieldedDepends", "shield"]
